438c4c607b5750e18997bce0617115b0d63039ad
irfankoprucu <irfankoprucu@gmail.com>
2020-05-20 00:35:13 +0300

    fluentassertions and autofixture added

--- ECommerce/ECommerce.Tests/CheckoutServiceTests.cs
+++ ECommerce/ECommerce.Tests/CheckoutServiceTests.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using AutoFixture;
 using ECommerce.Modals;
 using ECommerce.Repositories;
 using ECommerce.Services;
-using NUnit.Framework;
 using Moq;
+using NUnit.Framework;
+using System;
+using FluentAssertions;
 
 namespace ECommerce.Tests
 {
@@ -11,44 +13,59 @@ namespace ECommerce.Tests
   {
     private readonly DateTime today = DateTime.Now;
 
+    private Fixture _fixture;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _fixture = new Fixture();
+    }
+
     // Doğum günü bugün olan müşterilerimize %20 indirim uygulanır.
     [Test]
     public void it_should_apply_20_percent_discount_when_customers_birthday()
     {
       // Arrange
-      long customerId = 90;
+      var customerId = _fixture.Create<long>();
+
+      const decimal basketTotal = 100M;
+      const decimal expectedPaymentTotal = 90M;
 
       var customerRepo = new Mock<ICustomerRepo>();
 
-      var todayBornCustomer = new Customer
-      {
-        Birthdate = today
-      };
+      var todayBornCustomer = _fixture.Build<Customer>()
+                                      .With(x => x.Birthdate, today)
+                                      .With(x => x.IsVip, false)
+                                      .Create();
 
-      customerRepo.Setup(cr => cr.getById(customerId)).Returns(todayBornCustomer);
+      customerRepo
+        .Setup(cr => cr.getById(customerId))
+        .Returns(todayBornCustomer);
 
       var checkoutService = new CheckoutService(customerRepo.Object);
 
       // Act
-      const decimal basketTotal = 100;
-
-      decimal payedAmount = checkoutService.Checkout(customerId, basketTotal);
+      var payedAmount = checkoutService.Checkout(customerId, basketTotal);
 
       // Assert
-      Assert.AreEqual(90, payedAmount);
+      payedAmount.Should().Be(expectedPaymentTotal);
     }
-    
+
     [Test]
     public void it_should_not_apply_20_percent_discount_when_it_is_not_birthday()
     {
       // Arrange
+      var customerId = _fixture.Create<long>();
+
+      const decimal basketTotal = 100M;
+      const decimal expectedPaymentTotal = 110M;
+
       var customerRepo = new Mock<ICustomerRepo>();
-      long customerId = 90;
-      
-      var customer = new Customer
-      {
-        Birthdate = today.AddDays(-2)
-      };
+
+      var customer = _fixture.Build<Customer>()
+        .With(x => x.Birthdate, today.AddDays(-2))
+        .With(x => x.IsVip, false)
+        .Create();
 
       customerRepo
         .Setup(cr => cr.getById(customerId))
@@ -57,25 +74,27 @@ namespace ECommerce.Tests
       var checkoutService = new CheckoutService(customerRepo.Object);
 
       // Act
-      const decimal basketTotal = 100;
-
       decimal payedAmount = checkoutService.Checkout(customerId, basketTotal);
 
       // Assert
-      Assert.AreEqual(payedAmount, 110);
+      payedAmount.Should().Be(expectedPaymentTotal);
     }
 
     [Test]
     public void it_should_charge_10_lira_shipment_fee()
     {
       // Arrange
-      var customerRepo = new Mock<ICustomerRepo>();
-      long customerId = 90;
+      var customerId = _fixture.Create<long>();
 
-      var customer = new Customer
-      {
-        Birthdate = today.AddDays(-2)
-      };
+      const decimal basketTotal = 100M;
+      const decimal expectedPaymentTotal = 110M;
+
+      var customerRepo = new Mock<ICustomerRepo>();
+
+      var customer = _fixture.Build<Customer>()
+        .With(x => x.Birthdate, today.AddDays(-2))
+        .With(x => x.IsVip, false)
+        .Create();
 
       customerRepo
         .Setup(cr => cr.getById(customerId))
@@ -84,26 +103,27 @@ namespace ECommerce.Tests
       var checkoutService = new CheckoutService(customerRepo.Object);
 
       // Act
-      const decimal basketTotal = 100;
-
-      decimal payedAmount = checkoutService.Checkout(customerId, basketTotal);
+      var payedAmount = checkoutService.Checkout(customerId, basketTotal);
 
       // Assert
-      Assert.AreEqual(payedAmount, 110);
+      payedAmount.Should().Be(expectedPaymentTotal);
     }
 
     [Test]
     public void it_should_not_charge_shipment_fee_when_customer_is_vip()
     {
       // Arrange
-      var customerRepo = new Mock<ICustomerRepo>();
-      long customerId = 90;
+      var customerId = _fixture.Create<long>();
 
-      var customer = new Customer
-      {
-        Birthdate = today.AddDays(-2),
-        IsVip = true
-      };
+      const decimal basketTotal = 100M;
+      const decimal expectedPaymentTotal = 100M;
+
+      var customerRepo = new Mock<ICustomerRepo>();
+
+      var customer = _fixture.Build<Customer>()
+        .With(x => x.Birthdate, today.AddDays(-2))
+        .With(x => x.IsVip, true)
+        .Create();
 
       customerRepo
         .Setup(cr => cr.getById(customerId))
@@ -112,12 +132,10 @@ namespace ECommerce.Tests
       var checkoutService = new CheckoutService(customerRepo.Object);
 
       // Act
-      const decimal basketTotal = 100;
-
-      decimal payedAmount = checkoutService.Checkout(customerId, basketTotal);
+      var payedAmount = checkoutService.Checkout(customerId, basketTotal);
 
       // Assert
-      Assert.AreEqual(payedAmount, 100);
+      payedAmount.Should().Be(expectedPaymentTotal);
     }
   }
 }
