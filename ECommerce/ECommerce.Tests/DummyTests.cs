83914314c95b0a7031e2e3e9275f866876f2c28d
irfankoprucu <irfankoprucu@gmail.com>
2020-05-19 21:32:06 +0300

    Arrnge testing environment

---
+++ ECommerce/ECommerce.Tests/DummyTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+/*
+  Packages Installed:
+
+  NUnit
+  Moq
+  AutoFixture
+  FluentAssertions
+*/ 
+
+
+namespace ECommerce.Tests
+{
+  [TestFixture]
+  public class DummyTests
+  {
+    /// <summary>
+    /// method to be called immediately before each test is run
+    /// </summary>
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    /// <summary>
+    /// method to be called immediately after each test is run
+    /// </summary>
+    [TearDown]
+    public void TearDown()
+    {
+    }
+
+    [Test(Description = "This method make sure testing environment working correctly")]
+    public void It_should_configure_test_environment()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
